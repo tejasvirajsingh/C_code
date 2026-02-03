71ae5bba8355a77189b5c54a2497a70fdbf725fc
Tejasvi Raj Singh <tejasvirajsingh2003@gmail.com>
2026-02-03 20:40:31 +0530

    loop

---
+++ Loop/practice1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+   
+    for(int i=1; i<=10; i=i+3)
+    {
+        printf("MR Max\n");
+    }
+   
+   
+    return 0;
+
+}
---
+++ Loop/tableOf18.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    
+    for(int i=18;i<=180;i=i+18){
+        printf("%d ",i);
+    }
+    
+    return 0;
+
+}
